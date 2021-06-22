12e01f548b21e45b896de5d15f394915ab9a6d45
AnnaIvanova73 <allergictohumans73@gmail.com>
2021-06-22 04:11:37 +0300

    add in exams

--- JS-SECOND/0.1-exams/JS Advanced - Exam Prep 1 - 12.02.2021/01. SoftUni Administration/index.js
+++ JS-SECOND/0.1-exams/JS Advanced - Exam Prep 1 - 12.02.2021/01. SoftUni Administration/index.js
@@ -1,3 +1,60 @@
 function solve() {
+    const validateInput = (a, b) => !a || !b;
+    const appendChildren = (pr, children) => children.forEach(child => pr.appendChild(child));
+    const formatDate = (v) => v.split('-').join('/').split('T').join(' - ');
+    let elements = [];
 
+    document.querySelector('button').addEventListener('click', (e) => {
+        e.preventDefault();
+
+        let module = document.querySelector('select[name="lecture-module"]').value;
+        let name = document.querySelector('input[name="lecture-name"]').value;
+        let date = document.querySelector('input[name="lecture-date"]').value;
+
+        let select = document.querySelector('select');
+
+        if (validateInput(name, date) || module === 'Select module') {
+            return;
+        }
+        let currModelValue = `${select.value.toUpperCase()}-MODULE`;
+        let currModule = elements.filter(e => e.module === currModelValue);
+
+        if (currModule.length === 0) {
+            let divElementModule = createElement('div', null, ['class=module']);
+            let h3ElementNameModule = createElement('h3', currModelValue);
+            let ulElement = createElement('ul', null);
+
+            elements.push({module:currModelValue, ulElement, lists: [], divElementModule});
+            appendChildren(divElementModule, [h3ElementNameModule, ulElement]);
+            document.querySelector('.modules').appendChild(divElementModule);
+        }
+
+        let liElement = createElement('li', null, ['class=flex']);
+        let h4ElementName = createElement('h4', `${name} - ${formatDate(date)}`);
+        let btnDelete = createElement('button', 'Del', ['class=red']);
+        appendChildren(liElement, [h4ElementName, btnDelete]);
+
+        btnDelete.addEventListener('click', () => {
+            liElement.remove();
+            if (currModule[0].ulElement.childElementCount <= 0) {
+                currModule[0].divElementModule.remove();
+                elements = elements.filter(e => e.module !== module);
+            }
+        })
+        currModule = elements.filter(e => e.module === currModelValue);
+        date = formatDate(date);
+        currModule[0].lists.push({date:date, liElement});
+        currModule[0].lists.sort((a, b) => a.date.localeCompare(b.date)).forEach(el => currModule[0].ulElement.appendChild(el.liElement));
+    });
+
+    function createElement(type, text, attributes = []) {
+        let element = document.createElement(type);
+        if (text) {
+            element.textContent = text;
+        }
+        attributes.map(attr => attr.split('=')).forEach(([name, value]) => {
+            element.setAttribute(name, value);
+        })
+        return element;
+    }
 }
--- JS-SECOND/0.1-exams/JS Advanced Exam - 20 February 2021/2-story.js
+++ JS-SECOND/0.1-exams/JS Advanced Exam - 20 February 2021/2-story.js
@@ -8,71 +8,71 @@ class Story {
 
     get likes() {
         if (this._likes.length === 0) {
-            return `${this.title} has ${this._likes.length} likes`
+            return `${this.title} has ${this._likes.length} likes`;
         }
         if (this._likes.length === 1) {
             return `${this._likes[0]} likes this story!`
         }
 
-        return `${this._likes[0]} and ${this._likes.length - 1} others like this story!`
+        return `${this._likes[0]} and ${this._likes.length -1} others like this story!`;
     };
 
     like(username) {
-        let currUser = this._likes.filter(user => user === username);
+        let currUser = this._likes.filter(e => e === username);
         if (currUser.length > 0) {
-            throw new Error ("You can't like the same story twice!");
+            throw new Error(`You can't like the same story twice!`)
         }
-        if (this.creator === username) {
-            throw new Error ("You can't like your own story!");
+        if (username === this.creator) { //?
+            throw new Error(`You can't like your own story!`)
         }
         this._likes.push(username);
-        return `${username} liked ${this.title}!`
+        return `${username} liked ${this.title}!`;
     };
 
     dislike(username) {
-        let currUser = this._likes.filter(user => user === username);
+        let currUser = this._likes.filter(e => e === username);
         if (currUser.length === 0) {
-            throw new Error ("You can't dislike this story!")
+            throw new Error(`You can't dislike this story!`)
         }
-        this._likes = this._likes.filter(user => user !== username);
+
+        this._likes = this._likes.filter(e => e !== username);
         return `${username} disliked ${this.title}`
-    };
+    }
 
     comment(username, content, id) {
-        let currComment = this._comments.filter(comment => comment.id === id);
-        if ( currComment.length === 0 || typeof id === 'undefined') {
-            let currId = this._comments.length + 1;
-            this._comments.push({id:currId, username, content});
+        let currComment = this._comments.filter(e => e.id === id);
+
+        if (currComment.length === 0 || typeof id === 'undefined') {
+            let currentId = this._comments.length + 1;
+            this._comments.push({id: currentId, username: username, content: content})
             return `${username} commented on ${this.title}`
         }
-        currComment = this._comments.filter(comment => comment.id === id)[0];
-        if (!currComment['replies']) {
-            currComment.replies = [];
+        if (!currComment[0]['replies']) {
+            currComment[0].replies = [];
         }
-        let calcId = currComment.replies.length + 1;
-        let replyId = `${currComment.id}.${calcId}`
-        currComment.replies.push({id: replyId, username, content})
-        return `You replied successfully`
-    };
+
+        let replyCounter = currComment[0].replies.length + 1
+        let replyId = currComment[0].id + '.' + replyCounter;
+        currComment[0].replies.push({id: replyId, username: username, content: content});
+        return "You replied successfully";
+    }
 
     toString(type) {
-        let doSorting = {
+        let result = [`Title: ${this.title}`, `Creator: ${this.creator}`, `Likes: ${this._likes.length}`, 'Comments:'];
+        let sorting = {
             asc: (a, b) => a.id - b.id,
             desc: (a, b) => b.id - a.id,
-            username: (a, b) => a.username.localeCompare(b.username)
+            username: (a, b) => a.username.localeCompare(b.username),
         }
-        let result = [`Title: ${this.title}`, `Creator: ${this.creator}`, `Likes: ${this._likes.length}`,`Comments:` ]
-        this._comments.sort(doSorting[type]).forEach(e => {
+        this._comments.sort(sorting[type]).forEach(e => {
             result.push(`-- ${e.id}. ${e.username}: ${e.content}`);
-            if (e.replies) {
-                let rep = e.replies.sort(doSorting[type]).forEach(e => result.push(`--- ${e.id}. ${e.username}: ${e.content}`));
+            if (e['replies']) {
+                e.replies.sort(sorting[type]).forEach(r => result.push(`--- ${r.id}. ${r.username}: ${r.content}`))
             }
         })
-
         return result.join('\n');
     }
 }
-
 
 let art = new Story("My Story", "Anny");
 
@@ -96,13 +96,90 @@ console.log(art.comment("SAmmy", "Reply@", 1));
 
 console.log()
 
-console.log(art.toString('username'));
+console.log(art.toString('asc'));
 
 console.log()
 
 art.like("Zane");
 
 console.log(art.toString('desc'));
+
+
+// class Story {
+//     constructor(title, creator) {
+//         this.title = title;
+//         this.creator = creator;
+//         this._comments = [];
+//         this._likes = [];
+//     }
+//
+//     get likes() {
+//         if (this._likes.length === 0) {
+//             return `${this.title} has ${this._likes.length} likes`
+//         }
+//         if (this._likes.length === 1) {
+//             return `${this._likes[0]} likes this story!`
+//         }
+//
+//         return `${this._likes[0]} and ${this._likes.length - 1} others like this story!`
+//     };
+//
+//     like(username) {
+//         let currUser = this._likes.filter(user => user === username);
+//         if (currUser.length > 0) {
+//             throw new Error ("You can't like the same story twice!");
+//         }
+//         if (this.creator === username) {
+//             throw new Error ("You can't like your own story!");
+//         }
+//         this._likes.push(username);
+//         return `${username} liked ${this.title}!`
+//     };
+//
+//     dislike(username) {
+//         let currUser = this._likes.filter(user => user === username);
+//         if (currUser.length === 0) {
+//             throw new Error ("You can't dislike this story!")
+//         }
+//         this._likes = this._likes.filter(user => user !== username);
+//         return `${username} disliked ${this.title}`
+//     };
+//
+//     comment(username, content, id) {
+//         let currComment = this._comments.filter(comment => comment.id === id);
+//         if ( currComment.length === 0 || typeof id === 'undefined') {
+//             let currId = this._comments.length + 1;
+//             this._comments.push({id:currId, username, content});
+//             return `${username} commented on ${this.title}`
+//         }
+//         currComment = this._comments.filter(comment => comment.id === id)[0];
+//         if (!currComment['replies']) {
+//             currComment.replies = [];
+//         }
+//         let calcId = currComment.replies.length + 1;
+//         let replyId = `${currComment.id}.${calcId}`
+//         currComment.replies.push({id: replyId, username, content})
+//         return `You replied successfully`
+//     };
+//
+//     toString(type) {
+//         let doSorting = {
+//             asc: (a, b) => a.id - b.id,
+//             desc: (a, b) => b.id - a.id,
+//             username: (a, b) => a.username.localeCompare(b.username)
+//         }
+//         let result = [`Title: ${this.title}`, `Creator: ${this.creator}`, `Likes: ${this._likes.length}`,`Comments:` ]
+//         this._comments.sort(doSorting[type]).forEach(e => {
+//             result.push(`-- ${e.id}. ${e.username}: ${e.content}`);
+//             if (e.replies) {
+//                 let rep = e.replies.sort(doSorting[type]).forEach(e => result.push(`--- ${e.id}. ${e.username}: ${e.content}`));
+//             }
+//         })
+//
+//         return result.join('\n');
+//     }
+// }
+
 
 /*
 class Story {
